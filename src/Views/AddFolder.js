7819560d4aa4a49e28fe10d873d3362f9655e76d
tuyet2614 <nguyenanhtuyet2614@gmail.com>
2022-09-15 01:41:59 +0700

    score

--- src/Views/AddFolder.js
+++ src/Views/AddFolder.js
@@ -1,190 +1,199 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-    Text,
-    FlatList,
-    View,
-    Image,
-    TouchableHighlight,
-    TouchableOpacity,
+  Text,
+  FlatList,
+  View,
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
 } from 'react-native';
 import styles from '../component/Style';
 import ListWord from './ListWord';
 // import Vocabulary from '../Data/Data';
 import DataDb from '../service/DataDb';
 import vocabService from '../service/VocabService';
-import { openDatabase } from 'react-native-sqlite-storage';
+import {openDatabase} from 'react-native-sqlite-storage';
+// import ProgressCircle from 'react-native-progress-circle';
+// import {ViewPropTypes} from 'deprecated-react-native-prop-types';
+// import CircularProgress from 'react-native-circular-progress-indicator';
+import {AnimatedCircularProgress} from 'react-native-circular-progress';
+import {set} from 'react-native-reanimated';
 
-
-const db = openDatabase({
+const db = openDatabase(
+  {
     name: 'run.db',
     createFromLocation: '~vocabulary.db',
-}, null, null);
-
-const Item = ({ index, item, onPress, score }) => (
-    <TouchableOpacity onPress={onPress} style={styles.item}>
-        <View style={styles.index_group}>
-
-            <Text style={{ fontSize: 15, color: '#fff', marginLeft: 10, fontWeight: 'bold' }} >{index}</Text>
-        </View>
-
-        <Text style={styles.group}>{item.title}</Text>
-        <View>{score}</View>
-    </TouchableOpacity>
+  },
+  null,
+  null,
 );
 
-const Dashboard = ({ route, navigation }) => {
-    const [listFolder, setListFolder] = useState([])
-    const [listVocabulary, setListVocabulary] = useState([])
-    let listGroup = []
+const Item = ({index, item, onPress}) => (
+  <TouchableOpacity onPress={onPress} style={styles.item}>
+    {/* <View> */}
+    <View style={styles.index_group}>
+      <Text
+        style={{
+          fontSize: 15,
+          color: '#fff',
+          marginLeft: 10,
+          fontWeight: 'bold',
+        }}>
+        {index}
+      </Text>
+    </View>
+    <View style={{width: 230}}>
+      <Text style={[styles.group]}>{item.title}</Text>
+    </View>
+    {/* </View> */}
+
+    <View
+      style={{
+        backgroundColor: '#fff',
+        borderRadius: 100,
+        position: 'absolute',
+        right: 20,
+
+        justifyContent: 'center',
+      }}>
+      <AnimatedCircularProgress
+        size={50}
+        width={5}
+        fill={item.score}
+        tintColor="#D2691E"
+        backgroundColor="#89917e">
+        {fill => (
+          <Text style={{color: '#D2691E', fontSize: 20}}>{item.score}</Text>
+        )}
+      </AnimatedCircularProgress>
+    </View>
+  </TouchableOpacity>
+);
+
+const Dashboard = ({route, navigation}) => {
+  const [listFolder, setListFolder] = useState([]);
+  const [listVocabulary, setListVocabulary] = useState([]);
+  console.log(listFolder);
+  let listGroup = [];
+
+  listVocabulary.map(item =>
+    listGroup.includes(item.group) ? '' : listGroup.push(item.group),
+  );
+
+  const renderFolder = () => {
+    let exam = [];
+    vocabService
+      .getAll()
+      .then(res => {
+        res.data.map(item => (item.handle ? '' : exam.push(item)));
+        // console.log(res.data)
+        setListFolder(exam);
+      })
+      .catch(error => console.log(error));
+  };
+
+  const onSubmitFormHandler = async titleFolder => {
+    let data = {
+      title: titleFolder,
+      handle: false,
+      score: 0,
+    };
+
+    await vocabService
+      .create(data)
+      .then(res => {
+        setListFolder(pre => [res.data, ...pre]);
+        console.log('success fully');
+      })
+      .catch(error => console.log(error));
+  };
+
+  const getCategories = () => {
+    db.transaction(txn => {
+      txn.executeSql(
+        'SELECT * FROM vocabulary',
+
+        [],
+        (sqlTxn, res) => {
+          let len = res.rows.length;
+
+          if (len > 0) {
+            let results = [];
+            for (let i = 0; i < len; i++) {
+              let item = res.rows.item(i);
+
+              results.push({
+                id: item.id,
+                group: item.group,
+                title: item.title,
+                phonetic: item.phonetic,
+                eMean: item.eMean,
+                mean: item.mean,
+                img: item.img,
+                exam: item.exam,
+                trans: item.trans,
+              });
+            }
+
+            setListVocabulary(results);
+          }
+          // else (
+          //     setCategories([])
+          // )
+        },
+        error => {
+          console.log('error on getting categories ' + error.message);
+        },
+      );
+    });
+  };
+
+  useEffect(() => {
+    let listGroup = [];
+    getCategories();
 
     listVocabulary.map(item =>
-        listGroup.includes(item.group) ? '' : listGroup.push(item.group),
+      listGroup.includes(item.group) ? '' : listGroup.push(item.group),
     );
+    console.log(listGroup);
+    let exam = [];
+    listFolder.map(item => exam.push(item.title));
 
-    const renderFolder = () => {
-        let exam = []
-        vocabService.getAll().then(res => {
-            res.data.map(item => item.handle ? '' : exam.push(item))
-            // console.log(res.data)
-            setListFolder(exam)
-            console.log(exam)
+    listGroup.map(item =>
+      exam.includes(item)
+        ? ''
+        : setTimeout(function () {
+            //Put All Your Code Here, Which You Want To Execute After Some Delay Time.
+            onSubmitFormHandler(item);
+          }, 1000),
+    );
+    // listFolder.map(item => listGroup.includes(item.title) ? '': onSubmitFormHandler())
+    renderFolder();
+  }, []);
 
-        }).catch(error => console.log(error));
-    }
-
-    const onSubmitFormHandler = (titleFolder) => {
-        let data = {
-            "title": titleFolder,
-            "handle": false,
-            "score": 0
-        }
-
-        vocabService.create(data).
-            then(res => {
-                setListFolder((pre) => [res.data, ...pre])
-                console.log("success fully")
-            }).catch(error => console.log(error));
-
-
-    };
-
-    const getCategories = () => {
-        db.transaction(txn => {
-            txn.executeSql(
-                'SELECT * FROM vocabulary',
-
-                [],
-                (sqlTxn, res) => {
-                    console.log('categories retrieved successfully');
-                    let len = res.rows.length;
-
-                    if (len > 0) {
-                        let results = [];
-                        for (let i = 0; i < len; i++) {
-                            let item = res.rows.item(i);
-
-                            results.push({
-                                id: item.id,
-                                group: item.group,
-                                title: item.title,
-                                phonetic: item.phonetic,
-                                eMean: item.eMean,
-                                mean: item.mean,
-                                img: item.img,
-                                exam: item.exam,
-                                trans: item.trans
-                            });
-                        }
-
-                        setListVocabulary(results);
-                    }
-                    // else (
-                    //     setCategories([])
-                    // )
-                },
-                error => {
-                    console.log('error on getting categories ' + error.message);
-                },
-            );
-        });
-    };
-
-    const renderProgressBar = () => {
-        return (
-            <View style={{
-                borderRadius: 100,
-                width: 40,
-                height: 40
-            }}>
-
-
-                <View
-                    style={{
-                        width: '80%',
-                        height: 10,
-                        borderRadius: 20,
-                        backgroundColor: '#ffffff',
-                        alignSelf: 'center'
-                    }}>
-
-                    <Animated.View
-                        style={[
-                            {
-                                height: 10,
-                                borderRadius: 20,
-                                backgroundColor: COLORS.accent,
-                            },
-                            {
-                                width: progressAnim,
-                            },
-                        ]}></Animated.View>
-                </View>
-
-            </View>
-
-        );
-    };
-
-    useEffect(() => {
-        let listGroup = [];
-        getCategories()
-
-
-        // listVocabulary.map(item =>
-        //     listGroup.includes(item.group) ? '' : listGroup.push(item.group),
-        // );
-        // let exam = []
-        // listFolder.map(item => exam.push(item.title))
-
-        // listGroup.map(item => exam.includes(item) ? '' : onSubmitFormHandler(item))
-        // // listFolder.map(item => listGroup.includes(item.title) ? '': onSubmitFormHandler())
-        // renderFolder()
-        renderFolder()
-
-    }, [])
-
-    const renderItem = ({ index, item }) => {
-        return (
-            <Item
-                index={index + 1}
-                item={item}
-                onPress={() => {
-                    navigation.navigate('ListWord', { group: item.title, Vocabulary: listVocabulary });
-
-                }}
-                score={renderProgressBar}
-            />
-        );
-    };
+  const renderItem = ({index, item}) => {
     return (
-        <View style={styles.container}>
-            <FlatList
-                data={listFolder}
-                renderItem={renderItem}
-                keyExtractor={(item, index) => index}
-            />
-        </View>
+      <Item
+        index={index + 1}
+        item={item}
+        onPress={() => {
+          navigation.navigate('ListWord', {
+            group: item,
+            Vocabulary: listVocabulary,
+          });
+        }}
+      />
     );
+  };
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={listFolder}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index}
+      />
+    </View>
+  );
 };
 
 export default Dashboard;
--- src/Views/ListWord.js
+++ src/Views/ListWord.js
@@ -1,79 +1,88 @@
-import React, { useEffect } from 'react';
-import { Text, View, FlatList, TouchableOpacity, Image, ScrollView } from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from 'react-native';
 // import Vocabulary from '../Data/Data';
 import styles from '../component/Style';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faVolumeHigh} from '@fortawesome/free-solid-svg-icons';
 
 import Tts from 'react-native-tts';
 
-Tts.setIgnoreSilentSwitch("ignore");
-
+Tts.setIgnoreSilentSwitch('ignore');
 
 Tts.addEventListener('tts-start');
 Tts.addEventListener('tts-progress');
 Tts.addEventListener('tts-finish');
 Tts.addEventListener('tts-cancel');
 
-const ListWord = ({ route, navigation }) => {
-    const Vocabulary = route.params.Vocabulary
-    const vocabList = route.params.group;
+const ListWord = ({route, navigation}) => {
+  const Vocabulary = route.params.Vocabulary;
+  const vocabList = route.params.group.title;
+  const currentId = route.params.group.id;
+  console.log(currentId);
 
+  let listVocab = [];
 
-    let listVocab = [];
+  Vocabulary.map(item =>
+    item.group === vocabList ? listVocab.push(item) : '',
+  );
 
-    Vocabulary.map(item => item.group === vocabList ? listVocab.push(item) : '')
-
-
-
-    return (
-        <View style={{ backgroundColor: '#ffffff', flex: 1 }}>
-            <View>
-                <TouchableOpacity
-                    onPress={() => {
-                        navigation.navigate('Practice', {
-                            listVocab: listVocab,
-                            Vocabulary: Vocabulary
-                        });
-                    }}>
-                    <Image
-                        source={require('../component/images/practive.png')}
-                        style={styles.icon_practive}
-                    />
+  return (
+    <View style={{backgroundColor: '#ffffff', flex: 1}}>
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Practice', {
+              listVocab: listVocab,
+              Vocabulary: Vocabulary,
+              group: vocabList,
+              currentId: currentId,
+            });
+          }}>
+          <Image
+            source={require('../component/images/practive.png')}
+            style={styles.icon_practive}
+          />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <ScrollView>
+          {listVocab.map(item => (
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => {
+                navigation.navigate('DetailWord', {
+                  vocabGroup: listVocab,
+                  detail: item,
+                });
+              }}>
+              <View style={[styles.iconVolume, {marginRight: 100}]}>
+                <TouchableOpacity onPress={() => Tts.speak(item.title)}>
+                  <FontAwesomeIcon
+                    icon={faVolumeHigh}
+                    style={{color: '#ffffff'}}
+                    size={18}
+                  />
                 </TouchableOpacity>
-            </View>
-            <View>
-
-
-                <ScrollView>
-
-                    {listVocab.map(item =>
-                    (<TouchableOpacity style={styles.item} onPress={() => {
-                        navigation.navigate('DetailWord', {
-                            vocabGroup: listVocab,
-                            detail: item,
-
-                        });
-                    }}>
-                        <View style={[styles.iconVolume, { marginRight: 100 }]}>
-                            <TouchableOpacity onPress={() => Tts.speak(item.title)}>
-                                <FontAwesomeIcon
-                                    icon={faVolumeHigh}
-                                    style={{ color: '#ffffff' }}
-                                    size={18}
-                                />
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{ justifyContent: 'center', alignContent: 'center' }}>
-                            <Text style={styles.group}>{item.title}</Text>
-                            <Text style={{ fontSize: 15, color: '#000' }}>{item.phonetic}</Text>
-                        </View>
-                    </TouchableOpacity>)
-                    )}
-                </ScrollView>
-            </View>
-        </View>
-    );
+              </View>
+              <View style={{justifyContent: 'center', alignContent: 'center'}}>
+                <Text style={styles.group}>{item.title}</Text>
+                <Text style={{fontSize: 15, color: '#000'}}>
+                  {item.phonetic}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
 };
 
 export default ListWord;
